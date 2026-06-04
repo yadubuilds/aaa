b66603c11e3e37a5b21d05b152419618bcd321ba
yadubuilds <mail@yadusins.com>
2026-06-04 19:07:35 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "33013808"))
+API_HASH = os.getenv("API_HASH", "fa3ab424a80a33bf68a33d6f949fc167")
+STRING = os.getenv("STRING", "BAH3wDAAFMnzmYdv_LoHDtUA8GdpiphQYg_bP94qYRoHJ_n0rsNNDn5td_xHOSjk05lJl_ot15zyBpRy3JfUhONblD6pqQ76FaCPghabrxush3bqL_jPb91EzH6g5Afu99KZn-IAQ-4TbP9b1Pcn6e-GxaquvN0kJq4Ld_JzTR6Fg7Mt3WHJKoOjhlWs4dW4T-wxx2DW1l5nrd1EoJw2lT7rZVurVWhNkPnj0Vi_RTvE6umOsAuMdp21eVakjm_S_OI3Fk1TkYpgLVUJhz0cSl_5gyAvLSxDZIznAPu_BxW61T2HPhTswsZZHECyv6jdfs7rKuc26y0CUHR2oEAMXjBcJ48-dwAAAAHFV-cQAA")
+CMD_HNDLR = "."
